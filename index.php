a52761f49e884ef8e7c1942815d181e256afd2ee
Hari pandey <haripandey@Haris-MacBook-Pro-544.local>
2020-03-02 20:43:02 +0200

    I created new files called connection.php, result.php and style.css. I have also now created a database and linked it to the files.

--- index.php
+++ index.php
@@ -3,9 +3,22 @@
 <title>Homapage</title>
 <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+    <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+<link type="text/css" rel="stylesheet" href="style.css">
 <body>
+    <?php include 'connection.php';?>
+    <div class="img-container">
+        <img src="academy_logo.png" width="300" height="300" >
+    </div>
     
-    <input
+    <button type="button" class="btn btn-success btn-lg btn-block"><a href="result.php">Check your result</a></button>
+    <button type="button" class="btn btn-success btn-lg btn-block">Input the result</button>
+  <!--  <img src="academy_logo.png" id="logo">
+      <span id="school_name">Hari's Academy</span> -->
+ 
+    
     
     
 </body>
